3d38b71138d86cf1d25d58feb246b25fcaff36b9
Dean Silfen <dean.silfen@gmail.com>
2018-01-16 16:36:04 -0500

    Initial Commit

---
+++ NYPLAudiobookToolkit/NYPLAudiobookToolkit.h
@@ -0,0 +1,19 @@
+//
+//  NYPLAudiobookToolkit.h
+//  NYPLAudiobookToolkit
+//
+//  Created by Dean Silfen on 1/16/18.
+//  Copyright © 2018 Dean Silfen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NYPLAudiobookToolkit.
+FOUNDATION_EXPORT double NYPLAudiobookToolkitVersionNumber;
+
+//! Project version string for NYPLAudiobookToolkit.
+FOUNDATION_EXPORT const unsigned char NYPLAudiobookToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NYPLAudiobookToolkit/PublicHeader.h>
+
+
