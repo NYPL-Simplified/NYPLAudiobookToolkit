129746c15bc1a3c6e014d7b63d9e95bcde14fd8f
Dean Silfen <dean.silfen@gmail.com>
2018-02-23 10:05:24 -0500

    remove unused VC methods

--- NYPLAudiobookToolkit/UI/AudiobookTableOfContentsTableViewController.swift
+++ NYPLAudiobookToolkit/UI/AudiobookTableOfContentsTableViewController.swift
@@ -9,24 +9,14 @@
 import UIKit
 
 class AudiobookTableOfContentsTableViewController: UITableViewController {
+
     let dataSource: AudiobookTableOfContentsDataSource
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     public init(dataSource: AudiobookTableOfContentsDataSource) {
         self.dataSource = dataSource
         super.init(nibName: nil, bundle: nil)
         self.title = "Table Of Contents"
         self.dataSource.tableView = self.tableView
         self.tableView.dataSource = self.dataSource
-        
     }
     
     required init?(coder aDecoder: NSCoder) {
