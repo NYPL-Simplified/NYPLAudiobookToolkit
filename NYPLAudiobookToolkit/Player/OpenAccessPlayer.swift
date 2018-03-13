2972b06d7002efc2b926b4fcc6d9014ea3e79792
Dean Silfen <dean.silfen@gmail.com>
2018-03-13 17:35:23 -0400

    remove duplicate definition

--- NYPLAudiobookToolkit/Player/OpenAccessPlayer.swift
+++ NYPLAudiobookToolkit/Player/OpenAccessPlayer.swift
@@ -24,7 +24,6 @@ final class OpenAccessPlayer: NSObject, Player {
     }
     
     var delegate: PlayerDelegate?
-    var currentChapterLocation: ChapterLocation?
     func jumpToLocation(_ chapter: ChapterLocation) {
 
     }
