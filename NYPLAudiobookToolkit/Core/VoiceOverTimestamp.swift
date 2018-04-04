3cae9b366cef34c2d2acbfce3598f54ea8452504
Dean Silfen <dean.silfen@gmail.com>
2018-04-04 17:04:08 -0400

    Update voice over timestamp with localization

--- NYPLAudiobookToolkit/Core/VoiceOverTimestamp.swift
+++ NYPLAudiobookToolkit/Core/VoiceOverTimestamp.swift
@@ -15,14 +15,48 @@ class VoiceOverTimestamp: NSObject {
         let minutes = (interval / 60) % 60
         let hours = (interval / 3600)
         if hours > 0 {
-            return "\(hours) hours and \(minutes) minutes"
+            return self.stringsForHours(hours: hours, minutes: minutes)
         } else if minutes > 0 {
-            return "\(minutes) minutes"
+            return self.stringForMinutes(minutes: minutes)
         } else {
-            return "\(seconds) seconds"
+            return self.stringForSeconds(seconds: seconds)
         }
     }()
+
+    func stringForMinutes(minutes: Int) -> String {
+        let format: String
+        if minutes != 1 {
+            format = NSLocalizedString("%d minutes", bundle: Bundle.audiobookToolkit()!, value: "%d minutes", comment: "plural minutes with formatting")
+        } else {
+            format = NSLocalizedString("%d minute", bundle: Bundle.audiobookToolkit()!, value: "%d minute", comment: "singular minutes")
+        }
+        return String(format: format, minutes)
+    }
     
+    func stringForSeconds(seconds: Int) -> String {
+        let format: String
+        if seconds != 1 {
+            format = NSLocalizedString("%d seconds", bundle: Bundle.audiobookToolkit()!, value: "%d seconds", comment: "plural seconds with formatting")
+        } else {
+            format = NSLocalizedString("%d second", bundle: Bundle.audiobookToolkit()!, value: "%d second", comment: "singular minutes")
+        }
+        return String(format: format, seconds)
+    }
+
+    func stringsForHours(hours: Int, minutes: Int) -> String {
+        let format: String
+        if minutes != 1 && hours != 1 {
+            format = NSLocalizedString("%d hours and %d minutes", bundle: Bundle.audiobookToolkit()!, value: "%d hours and %d minutes", comment: "plural hours and minutes with formatting")
+        } else if minutes != 1 && hours == 1 {
+            format = NSLocalizedString("%d hour and %d minutes", bundle: Bundle.audiobookToolkit()!, value: "%d hour and %d minutes", comment: "singular hours and plural minutes with formatting")
+        } else if minutes == 1 && hours != 1 {
+            format = NSLocalizedString("%d hours and %d minute", bundle: Bundle.audiobookToolkit()!, value: "%d hours and %d minute", comment: "plural hours and singular minutes with formatting")
+        } else {
+            format = NSLocalizedString("%d hour and %d minute", bundle: Bundle.audiobookToolkit()!, value: "%d hour and %d minute", comment: "singular hours and minutes with formatting")
+        }
+        return String(format: format, hours, minutes)
+    }
+
     private let timeInterval: TimeInterval
     init(timeInterval: TimeInterval) {
         self.timeInterval = timeInterval
