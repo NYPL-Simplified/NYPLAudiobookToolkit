16435242d85c5492d677310d967f8cd3e3fbce5c
Dean Silfen <dean.silfen@gmail.com>
2018-04-09 12:29:38 -0400

    Update UI elements to respond to highlighted events

--- NYPLAudiobookToolkit/UI/TextWithImageView.swift
+++ NYPLAudiobookToolkit/UI/TextWithImageView.swift
@@ -9,7 +9,7 @@
 import UIKit
 import PureLayout
 
-class TextOverImageView: UIView {
+class TextOverImageView: HighlightedUIControl {
     
     var text: String? {
         get {
@@ -53,6 +53,9 @@ class TextOverImageView: UIView {
     }
     
     func setup() {
+        self.accessibilityTraits = UIAccessibilityTraitButton
+        self.isAccessibilityElement = true
+
         self.addSubview(self.backgroundImageView)
         self.backgroundImageView.accessibilityIdentifier = "TextOverImageView.backgroundImageView"
         self.backgroundImageView.contentMode = .scaleAspectFit
@@ -72,7 +75,7 @@ class TextOverImageView: UIView {
         self.subtextLabel.autoPinEdge(.top, to: .bottom, of: self.textLabel, withOffset: -6)
         self.subtextLabel.autoAlignAxis(.vertical, toSameAxisOf: self.textLabel)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
