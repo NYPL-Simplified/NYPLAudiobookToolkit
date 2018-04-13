8c89d40b82a4892c4a946081d4477a72f5198472
Dean Silfen <dean.silfen@gmail.com>
2018-04-13 13:59:18 -0400

    Update Payload to new book

--- NYPLAudiobookToolkitDemoApp/ViewController.swift
+++ NYPLAudiobookToolkitDemoApp/ViewController.swift
@@ -20,75 +20,7 @@ class ViewController: UIViewController {
 //        }
 //        guard let json = possibleJson else { return }
         let json = """
-
-
-{
-  "spine": [
-    {
-      "title": "Track 1",
-      "findaway:sequence": 1,
-      "href": null,
-      "duration": 3749.386,
-      "findaway:part": 0,
-      "type": "audio/mpeg"
-    },
-    {
-      "title": "Track 2",
-      "findaway:sequence": 2,
-      "href": null,
-      "duration": 3005.262,
-      "findaway:part": 0,
-      "type": "audio/mpeg"
-    },
-    {
-      "title": "Track 3",
-      "findaway:sequence": 3,
-      "href": null,
-      "duration": 3949.85,
-      "findaway:part": 0,
-      "type": "audio/mpeg"
-    },
-    {
-      "title": "Track 4",
-      "findaway:sequence": 4,
-      "href": null,
-      "duration": 2859.943,
-      "findaway:part": 0,
-      "type": "audio/mpeg"
-    }
-  ],
-  "@context": [
-    "http://readium.org/webpub/default.jsonld",
-    {
-      "findaway": "http://librarysimplified.org/terms/third-parties/findaway.com/"
-    }
-  ],
-  "links": [
-    {
-      "href": "http://book-covers.nypl.org/scaled/300/Content%20Cafe/ISBN/9780743585149/cover.jpg",
-      "rel": "cover"
-    }
-  ],
-  "metadata": {
-    "language": "en",
-    "title": "Star Trek: The Original Series: Vulcan's Soul #1: Exodus",
-    "encrypted": {
-      "findaway:accountId": "3M",
-      "findaway:checkoutId": "5ab415fa307372541b9ce044",
-      "findaway:sessionKey": "49fc2f9b-9d6a-4865-9e8e-22af3b44f145",
-      "findaway:fulfillmentId": "33732",
-      "findaway:licenseId": "579b6aabb692b15832c06860",
-      "scheme": "http://librarysimplified.org/terms/drm/scheme/FAE"
-    },
-    "authors": [
-      "Susan Shwartz",
-      "Josepha Sherman"
-    ],
-    "duration": 13564.440999999999,
-    "identifier": "urn:librarysimplified.org/terms/id/Bibliotheca%20ID/eb5ucz9",
-    "@type": "http://bib.schema.org/Audiobook"
-  }
-}
+{"spine": [{"title": "Track 1", "findaway:sequence": 1, "href": null, "duration": 3749.386, "findaway:part": 0, "type": "audio/mpeg"}, {"title": "Track 2", "findaway:sequence": 2, "href": null, "duration": 3005.262, "findaway:part": 0, "type": "audio/mpeg"}, {"title": "Track 3", "findaway:sequence": 3, "href": null, "duration": 3949.85, "findaway:part": 0, "type": "audio/mpeg"}, {"title": "Track 4", "findaway:sequence": 4, "href": null, "duration": 2859.943, "findaway:part": 0, "type": "audio/mpeg"}], "@context": ["http://readium.org/webpub/default.jsonld", {"findaway": "http://librarysimplified.org/terms/third-parties/findaway.com/"}], "links": [{"href": "http://book-covers.nypl.org/scaled/300/Content%20Cafe/ISBN/9780743585149/cover.jpg", "rel": "cover"}], "metadata": {"language": "en", "title": "Star Trek: The Original Series: Vulcan's Soul #1: Exodus", "encrypted": {"findaway:accountId": "3M", "findaway:checkoutId": "5ad0eea930737269dc9ce04b", "findaway:sessionKey": "81fca5be-2107-4beb-9676-1810645e7e6c", "findaway:fulfillmentId": "33732", "findaway:licenseId": "579b6aabb692b15832c06860", "scheme": "http://librarysimplified.org/terms/drm/scheme/FAE"}, "authors": ["Susan Shwartz", "Josepha Sherman"], "duration": 13564.440999999999, "identifier": "urn:librarysimplified.org/terms/id/Bibliotheca%20ID/eb5ucz9", "@type": "http://bib.schema.org/Audiobook"}}
 """
         guard let data = json.data(using: String.Encoding.utf8) else { return }
         let possibleJson = try? JSONSerialization.jsonObject(with: data, options: [])
