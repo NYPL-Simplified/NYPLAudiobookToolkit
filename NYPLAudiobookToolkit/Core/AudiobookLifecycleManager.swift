c7c2279a0be66e4402744993ba670e904cdf3661
Dean Silfen <dean.silfen@gmail.com>
2018-05-08 12:04:35 -0400

    prepare AudiobookLifecycleManager to have hooks for new listeners

--- NYPLAudiobookToolkit/Core/AudiobookLifecycleManager.swift
+++ NYPLAudiobookToolkit/Core/AudiobookLifecycleManager.swift
@@ -11,26 +11,32 @@ import UIKit
 import AVFoundation
 
 /// Delegate to be notified when the state of the lifecycle manager has changed
-@objc public protocol AudiobookLifecycleManagerDelegate: class {
-
-    /**
-     General notifications about the state of the manager.
-    */
-    func audiobookLifecycleManagerDidUpdate(_ audiobookLifecycleManager: AudiobookLifeCycleManager)
-}
+//@objc public protocol AudiobookLifecycleManagerDelegate: class {
+//
+//    /**
+//     General notifications about the state of the manager.
+//    */
+//    func audiobookLifecycleManagerDidUpdate(_ audiobookLifecycleManager: AudiobookLifeCycleManager)
+//}
 
 /// Implementers of this protocol should hook into Lifecycle events in AppDelegate.swift.
 /// They should also listen to notifications found in AudioEngine in order to update their internal state.
 /// This is a wrapper around the stateful aspects of AudioEngine and avoids objects listening to NSNotifications directly.
 @objc public protocol AudiobookLifeCycleManager: class {
-    var audioEngineDatabaseHasBeenVerified: Bool { get }
     func didFinishLaunching()
     func didEnterBackground()
     func willTerminate()
-    func registerDelegate(_ delegate: AudiobookLifecycleManagerDelegate)
-    func removeDelegate(_ delegate: AudiobookLifecycleManagerDelegate)
+//    func registerDelegate(_ delegate: AudiobookLifecycleManagerDelegate)
+//    func removeDelegate(_ delegate: AudiobookLifecycleManagerDelegate)
 }
 
+@objc public protocol AudiobookLifecycleListener: class {
+    func didFinishLaunching()
+    func didEnterBackground()
+    func willTerminate()
+    func handleBackgroundURLSession(for identifier: String, completionHandler: @escaping () -> Void) -> Bool
+    init()
+}
 
 /// Hooks into life cycle events for AppDelegate.swift. Listens to notifcations from
 /// AudioEngine to ensure other objects know when it is safe to perform operations on
@@ -40,11 +46,12 @@ public class DefaultAudiobookLifecycleManager: NSObject, AudiobookLifeCycleManag
      The shared instance of the lifecycle manager intended for usage throughout the framework.
      */
     public static let shared = DefaultAudiobookLifecycleManager()
-    private var delegates: NSHashTable<AudiobookLifecycleManagerDelegate> = NSHashTable(options: [NSPointerFunctions.Options.weakMemory])
-    public var audioEngineDatabaseHasBeenVerified: Bool {
-        return _audioEngineDatabaseHasBeenVerified
-    }
-    private var _audioEngineDatabaseHasBeenVerified = false
+//    private var delegates: NSHashTable<AudiobookLifecycleManagerDelegate> = NSHashTable(options: [NSPointerFunctions.Options.weakMemory])
+//    public var audioEngineDatabaseHasBeenVerified: Bool {
+//        return _audioEngineDatabaseHasBeenVerified
+//    }
+//    private var _audioEngineDatabaseHasBeenVerified = false
+    private var listeners = [AudiobookLifecycleListener]()
     private override init() {
         super.init()
 //        NotificationCenter.default.addObserver(
@@ -53,46 +60,62 @@ public class DefaultAudiobookLifecycleManager: NSObject, AudiobookLifeCycleManag
 //            name: NSNotification.Name.FAEDatabaseVerificationComplete,
 //            object: nil
 //        )
-    }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(self)
-    }
-
-    public func registerDelegate(_ delegate: AudiobookLifecycleManagerDelegate) {
-        self.delegates.add(delegate)
-    }
-    
-    public func removeDelegate(_ delegate: AudiobookLifecycleManagerDelegate) {
-        self.delegates.remove(delegate)
-    }
-
-    @objc public func audioEngineDatabaseVerificationStatusHasBeenUpdated(_ notification: NSNotification) {
-        self._audioEngineDatabaseHasBeenVerified = true
-        self.delegates.allObjects.forEach { (delegate) in
-            delegate.audiobookLifecycleManagerDidUpdate(self)
+        let FindawayListenerClass = NSClassFromString("NYPLAEToolkit.FindawayAudiobookLifecycleListener") as? AudiobookLifecycleListener.Type
+        let findawayListener = FindawayListenerClass?.init()
+        if let listener = findawayListener {
+            self.listeners.append(listener)
         }
     }
+
+//    public func registerDelegate(_ delegate: AudiobookLifecycleManagerDelegate) {
+//        self.delegates.add(delegate)
+//    }
+//
+//    public func removeDelegate(_ delegate: AudiobookLifecycleManagerDelegate) {
+//        self.delegates.remove(delegate)
+//    }
+//
+//    @objc public func audioEngineDatabaseVerificationStatusHasBeenUpdated(_ notification: NSNotification) {
+////        self._audioEngineDatabaseHasBeenVerified = true
+//        self.delegates.allObjects.forEach { (delegate) in
+//            delegate.audiobookLifecycleManagerDidUpdate(self)
+//        }
+//    }
 }
 
 extension DefaultAudiobookLifecycleManager {
     public func didFinishLaunching () {
 //        FAEAudioEngine.shared()?.didFinishLaunching()
 //        FAELogEngine.setLogLevel(.none)
+        self.listeners.forEach { (listener) in
+            listener.didFinishLaunching()
+        }
     }
     
     public func didEnterBackground () {
 //        FAEAudioEngine.shared()?.didEnterBackground()
+        self.listeners.forEach { (listener) in
+            listener.didEnterBackground()
+        }
     }
     
     public func willTerminate () {
 //        FAEAudioEngine.shared()?.willTerminate()
+        self.listeners.forEach { (listener) in
+            listener.willTerminate()
+        }
     }
     
     public func handleEventsForBackgroundURLSession(for identifier: String, completionHandler: @escaping () -> Void) {
-        if identifier.contains("FWAE") {
+        for listener in self.listeners {
+            let didHandle = listener.handleBackgroundURLSession(for: identifier, completionHandler: completionHandler)
+            if didHandle {
+                break
+            }
+        }
+//        if identifier.contains("FWAE") {
 //            FAEAudioEngine.shared()?.didFinishLaunching()
 //            FAEAudioEngine.shared()?.downloadEngine?.addCompletionHandler(completionHandler, forSession: identifier)
-        }
+//        }
     }
 }
