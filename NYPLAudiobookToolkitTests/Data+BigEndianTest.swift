0b6ec34effe22a8ae3b0c9c41e615f73623e5b99
Kyle Sakai <kyles@evidentpoint.com>
2020-04-08 15:51:03 -0700

    Fixing bad byte length check

---
+++ NYPLAudiobookToolkitTests/Data+BigEndianTest.swift
@@ -0,0 +1,46 @@
+import Foundation
+import XCTest
+
+@testable import NYPLAudiobookToolkit
+
+fileprivate let emptyData = Data.init(bytes: [])
+fileprivate let int32 = Data.init(bytes: [0, 0, 0, 1])
+fileprivate let int64 = Data.init(bytes: [0, 0, 0, 0, 0, 0, 0, 1])
+fileprivate let int32Offset1 = Data.init(bytes: [255, 0, 0, 0, 1])
+fileprivate let int64Offset1 = Data.init(bytes: [255, 0, 0, 0, 0, 0, 0, 0, 1])
+
+class DataBigEndianTest: XCTestCase {
+  func testBigEndianUInt32() {
+    var num: UInt32 = 0
+    XCTAssertThrowsError(try emptyData.bigEndianUInt32())
+    XCTAssertThrowsError(try int64.bigEndianUInt32())
+    XCTAssertNoThrow(num = try int32.bigEndianUInt32())
+    XCTAssertEqual(num, 1)
+  }
+    
+  func testBigEndianUInt64() {
+    var num: UInt64 = 0
+    XCTAssertThrowsError(try emptyData.bigEndianUInt64())
+    XCTAssertThrowsError(try int64Offset1.bigEndianUInt64())
+    XCTAssertNoThrow(num = try int64.bigEndianUInt64())
+    XCTAssertEqual(num, 1)
+  }
+    
+  func testBigEndianUInt32Offset() {
+    var num: UInt32 = 0
+    XCTAssertThrowsError(try emptyData.bigEndianUInt32At(offset: 1))
+    XCTAssertNoThrow(num = try int32Offset1.bigEndianUInt32At(offset: 1))
+    XCTAssertEqual(num, 1)
+    XCTAssertNoThrow(num = try int32.bigEndianUInt32At(offset: 0))
+    XCTAssertEqual(num, 1)
+  }
+    
+  func testBigEndianUInt64Offset() {
+    var num: UInt64 = 0
+    XCTAssertThrowsError(try emptyData.bigEndianUInt64At(offset: 1))
+    XCTAssertNoThrow(num = try int64Offset1.bigEndianUInt64At(offset: 1))
+    XCTAssertEqual(num, 1)
+    XCTAssertNoThrow(num = try int64.bigEndianUInt64At(offset: 0))
+    XCTAssertEqual(num, 1)
+  }
+}
