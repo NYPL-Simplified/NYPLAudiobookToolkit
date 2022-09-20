0781937c4fa61bc7106248811d29ea98f65858c0
Ernest Fan <ernestf@evidentpoint.com>
2022-09-20 16:37:21 -0700

    Add NYPLAudiobookBookmark dictionary representation and NYPLAudiobookBookmarkFactory make function

--- NYPLAudiobookToolkit/Bookmarks/NYPLAudiobookBookmark.swift
+++ NYPLAudiobookToolkit/Bookmarks/NYPLAudiobookBookmark.swift
@@ -2,7 +2,7 @@ import Foundation
 import NYPLUtilities
 
 // TODO: Decide if this should be a class or struct when implementation is completed
-public final class NYPLAudiobookBookmark: NYPLBookmark {
+@objc public final class NYPLAudiobookBookmark: NSObject, NYPLBookmark, Codable {
   public let title: String?
   public let chapter: UInt
   public let part: UInt
@@ -16,6 +16,7 @@ public final class NYPLAudiobookBookmark: NYPLBookmark {
   
   // MARK: Init
   
+  @objc
   public init(title: String? = nil,
               chapter: UInt,
               part: UInt,
@@ -71,7 +72,59 @@ public final class NYPLAudiobookBookmark: NYPLBookmark {
               creationTime: creationTime)
   }
   
-  // MARK: Serialize
+  /// Initialize from a dictionary representation, usually derived from
+  /// the `NYPLBookRegistry`.
+  ///
+  /// - Parameter dictionary: Dictionary representation of the bookmark. See
+  /// `NYPLBookmarkDictionaryRepresentation` for valid keys.
+  @objc public convenience init?(dictionary:NSDictionary) {
+    guard let chapter = dictionary[CodingKeys.chapter] as? UInt,
+          let part = dictionary[CodingKeys.part] as? UInt,
+          let duration = dictionary[CodingKeys.duration] as? TimeInterval,
+          let time = dictionary[CodingKeys.time] as? TimeInterval,
+          let audiobookId = dictionary[CodingKeys.audiobookId] as? String,
+          let creationTime = dictionary[CodingKeys.creationTime] as? Date else {
+      return nil
+    }
+    
+    
+    self.init(title: dictionary[CodingKeys.title] as? String,
+              chapter: chapter,
+              part: part,
+              duration: duration,
+              time: time,
+              audiobookId: audiobookId,
+              annotationId: dictionary[CodingKeys.annotationId] as? String,
+              device: dictionary[CodingKeys.device] as? String,
+              creationTime: creationTime)
+  }
+  
+  // MARK: Representation
+  
+  @objc public var dictionaryRepresentation:NSDictionary {
+    let dict: NSMutableDictionary = [
+      CodingKeys.chapter: self.chapter,
+      CodingKeys.part: self.part,
+      CodingKeys.duration: self.duration,
+      CodingKeys.time: self.time,
+      CodingKeys.audiobookId: self.audiobookId,
+      CodingKeys.creationTime: self.creationTime,
+    ]
+    
+    if let title = title {
+      dict[CodingKeys.title] = title
+    }
+    
+    if let annotationId = annotationId {
+      dict[CodingKeys.annotationId] = annotationId
+    }
+    
+    if let device = device {
+      dict[CodingKeys.device] = device
+    }
+    
+    return dict
+  }
   
   // Serialize the bookmark for posting to server and storing in local storage
   public func serializableRepresentation(forMotivation motivation: NYPLBookmarkSpec.Motivation,
